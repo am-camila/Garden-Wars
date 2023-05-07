5c3a0413323622840f8eceb6e762a20691b38d45
am-camila <marchinicamila94@gmail.com>
2023-05-07 20:46:17 -0300

    Add random spawning of the enemies

--- src/models/Enemy/Enemy.gd
+++ src/models/Enemy/Enemy.gd
@@ -4,16 +4,13 @@ extends KinematicBody2D
 var player
 export var speed = 3500
 
-func _ready():
-	pass # Replace with function body.
-
-
 func set_values(player):
 	self.player = player
 
-
-
 func _process(delta):
+	if player == null:
+		return
+		
 	var direction = (player.position - position).normalized()
 	var movement = direction * speed * delta
 	#if speed < 3000:
--- src/models/Main/Main.gd
+++ src/models/Main/Main.gd
@@ -1,13 +1,23 @@
 extends Node
 
-
-
+export var enemy_scene: PackedScene
+onready var player = $Player
+var enemy_count = 0
+export var max_enemies = 10
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Enemy.set_values($Player)
-	$Enemy2.set_values($Player)
+	randomize()
+	$EnemiesTimer.start()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# Called every time the timer times out.
+func _on_EnemiesTimer_timeout():
+	if enemy_count >= max_enemies:
+		return
+	var enemy = enemy_scene.instance()
+	enemy.set_values(player)
+	
+	var enemy_spawn_location = get_node("EnemiesPath/EnemiesPathLocation")
+	enemy_spawn_location.offset = randi()
+	enemy.position = enemy_spawn_location.position
+	add_child(enemy)
+	enemy_count+=1
